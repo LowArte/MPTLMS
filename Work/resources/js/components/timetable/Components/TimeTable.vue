ac1327a9ce5137db08d45716e76a5fd351895031
SurgeonMPT <artem160920@mail.ru>
2019-11-07 22:00:09 +0300

    fix_Show_Timetable

--- Work/resources/js/components/timetable/Components/TimeTable.vue
+++ Work/resources/js/components/timetable/Components/TimeTable.vue
@@ -2,9 +2,9 @@
   <v-container fluid>
     <v-row align="center">
       <v-container>
-        <v-autocomplete v-model="departamet" label="Отделения" solo :items="departaments"></v-autocomplete>
-        <v-autocomplete v-model="group" label="Группа" solo :items="groups"></v-autocomplete>
-        <v-container class="pa-0 align-self-center" d-flex>
+        <v-autocomplete v-model="departamet" label="Отделения" solo :items="arrdepartaments"></v-autocomplete>
+        <v-autocomplete v-model="group" label="Группа" solo :items="groups.group_name"></v-autocomplete>
+        <!-- <v-container class="pa-0 align-self-center" d-flex>
           <v-row sm="2" md="0" class="pa-0 justify-center">
             <v-col class="pa-0 d-flex justify-center" sm="2" md="0">
               <v-btn
@@ -14,11 +14,44 @@
               >{{ hidden ? 'Числитель' : 'Знаменатель' }}</v-btn>
             </v-col>
           </v-row>
-        </v-container>
+        </v-container>-->
       </v-container>
     </v-row>
     <v-divider class="ma-0"></v-divider>
-    <v-data :items="items[group]" :items-per-page.sync="itemsPerPage">
+    <v-data :items="schedule.schedule" :items-per-page.sync="itemsPerPage">
+      <v-row>
+        <v-col v-for="item in day" :key="item" cols="12" sm="6" md="2" lg="2">
+          <v-card>
+            <v-card-title class="subtitle-1 mb-0 pb-0" style="color: #FF3D00;">{{item}}</v-card-title>
+            <v-list dense>
+              <v-list-item>Здание: {{schedule[item].Place}}</v-list-item>
+              <v-divider class="my-2"></v-divider>
+              <div v-for="(n,i) in 7" :key="i" class="ma-0 pa-0">
+                <v-list
+                  class="ma-2 pa-0"
+                  v-if="schedule[item][n].Lesson != null && schedule[item][n].Lesson != ''"
+                >
+                  <v-list-item
+                    class="mt-0 mb-0 pt-0 pb-0"
+                  >{{n}} пара - {{callSchedule[0].call_schedule[n]}}</v-list-item>
+                  <v-list-item class="mt-0 mb-0 pt-0 pb-0">{{schedule[item][n].Lesson}}</v-list-item>
+                  <v-list-item class="mt-0 mb-0 pt-0 pb-0">{{schedule[item][n].Teacher }}</v-list-item>
+                </v-list>
+                <v-list class="ma-0 pa-0" v-else>
+                  <v-list-item
+                    class="mt-0 mb-0 pt-0 pb-0"
+                  >{{n}} пара - {{callSchedule[0].call_schedule[n]}}</v-list-item>
+                  <v-list-item class="mt-0 mb-0 pt-0 pb-0">Свободная пара</v-list-item>
+                </v-list>
+
+                <v-divider class="my-2"></v-divider>
+              </div>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-data>
+    <!-- <v-data :items="items[group]" :items-per-page.sync="itemsPerPage">
       <template v-slot:default="props">
         <v-row>
           <v-col v-for="item in props.items" :key="item.name" cols="12" sm="6" md="2" lg="2">
@@ -89,7 +122,13 @@
           </v-col>
         </v-row>
       </template>
-    </v-data>
+    </v-data>-->
+    <!-- {{this.groups}} -->
+    <br />
+    {{this.callSchedule}}
+    <br />
+    {{this.schedule}}
+    <br />
   </v-container>
 </template>
 
@@ -102,23 +141,31 @@ export default {
     itemsPerPageOptions: [6],
     itemsPerPage: 6,
     Datetime: "00.00.0000",
+
+    arrgroups: [],
+    arrdepartaments: [],
+    departament: null,
+
+    group: null,
+
+    day: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
   }),
-  props:{
-    schedule:{
+  props: {
+    schedule: {
       type: String,
-      default:null
+      default: null
     },
-    callSchedule:{
+    callSchedule: {
       type: String,
-      default:null
+      default: null
     },
-    groups:{
+    groups: {
       type: String,
-      default:null
+      default: null
     },
-    departaments:{
-      type:String,
-      default:null
+    departaments: {
+      type: String,
+      default: null
     }
   },
   methods: {
@@ -127,32 +174,54 @@ export default {
       return;
     }
   },
-  mounted:function() {
-      var currDate = new Date()
-      var hours = currDate.getHours()
-      var minutes = currDate.getMinutes()
-      var seconds = currDate.getSeconds()
-      if (hours <= 9) {
-        hours = "0" + hours
-      }
-      if (minutes <= 9) {
-        minutes = "0" + minutes
-      }
-      if (seconds <= 9) {
-        seconds = "0" + seconds
-      }
-      this.Datetime = hours + ":" + minutes + ":" + seconds
-      if(this.schedule=="")
-      {
-        console.log("Current schedule is empty")
-      }
-      else{
-        console.log(JSON.parse(this.schedule))
-      }
-      console.log(JSON.parse(this.groups))
-      console.log(JSON.parse(this.callSchedule))
-      console.log(JSON.parse(this.departaments))
+  mounted: function() {
+    var currDate = new Date();
+    var hours = currDate.getHours();
+    var minutes = currDate.getMinutes();
+    var seconds = currDate.getSeconds();
+    if (hours <= 9) {
+      hours = "0" + hours;
+    }
+    if (minutes <= 9) {
+      minutes = "0" + minutes;
+    }
+    if (seconds <= 9) {
+      seconds = "0" + seconds;
+    }
+    this.Datetime = hours + ":" + minutes + ":" + seconds;
 
+    console.log(JSON.parse(this.groups));
+    console.log(JSON.parse(this.callSchedule));
+    console.log(JSON.parse(this.departaments));
+
+    //Отделения
+    this.departaments = JSON.parse(this.departaments);
+    this.departaments = JSON.parse(this.departaments.departaments);
+    this.arrdepartaments = [];
+    for (var i = 0; i < this.departaments.length; i++)
+      this.arrdepartaments.push(this.departaments[i].dep_name_full);
+
+    //Группы
+    this.groups = JSON.parse(this.groups);
+    this.group = this.groups.group_name;
+
+    //Расписание звонков
+    this.callSchedule = JSON.parse(this.callSchedule);
+    for (var i = 0; i < this.callSchedule.length; i++)
+      this.callSchedule[i].call_schedule = JSON.parse(
+        this.callSchedule[i].call_schedule
+      );
+
+    //Расписание занятий
+    if (this.schedule == "") {
+      console.log("Current schedule is empty");
+    } else {
+      console.log(JSON.parse(this.schedule));
+      this.schedule = JSON.parse(this.schedule);
+      this.schedule = JSON.parse(this.schedule.schedule);
+      // this.schedule['Понедельник']["1"].Lesson = ["1","2"];
+    }
   }
+  //Непонятно где приходят отделения и где приходят группы
 };
 </script>
