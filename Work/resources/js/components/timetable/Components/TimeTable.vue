9be8cd234de5113ea5068689c615729abf953ee0
smetaninartem2001 <47990465+smetaninartem2001@users.noreply.github.com>
2019-11-08 14:58:05 +0300

    Revert "fix"
    This reverts commit 2444124043fff3d7f0afb076ed533abb12b113b3.

--- Work/resources/js/components/timetable/Components/TimeTable.vue
+++ Work/resources/js/components/timetable/Components/TimeTable.vue
@@ -52,7 +52,6 @@
       </v-row>
     </v-data>
     {{places}}
-    
     <br />
     {{callSchedule}}
     
--- Work/resources/views/components/timetable.blade.php
+++ Work/resources/views/components/timetable.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <v-container fluid grid-list-md text-xs-center>
-    <timetable place="{{$place ?? null }}" call="{{$call ?? null }}" schedule="{{$schedule ?? null}}" groups ="{{$groups ?? null}}" departaments="{{$dep ?? null}}"></timetable>
+    <timetable place="{{$places ?? null }}" call="{{$call ?? null }}" schedule="{{$schedule ?? null}}" groups ="{{$groups ?? null}}" departaments="{{$dep ?? null}}"></timetable>
 </v-container>
 
 @endsection
