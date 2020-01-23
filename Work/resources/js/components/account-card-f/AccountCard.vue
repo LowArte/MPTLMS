bde9e4529cb81bbb08e2d4e741293050ef86e321
smetaninartem2001 <47990465+smetaninartem2001@users.noreply.github.com>
2020-01-23 17:45:19 +0300

    Fix

---
+++ Work/resources/js/components/account-card-f/AccountCard.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    v-layout.row.wrap
+        v-card(class="mx-auto" max-width="400")
+            v-list-item(two-line)
+                v-list-item-avatar
+                v-list-item-content
+                    v-list-item-title.headline {{name}}
+                    v-list-item-subtitle Должность: {{}}
+            v-divider
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        name: "Фамилия Имя Отчество",
+        post: "Наименование должности"
+    };
+  }
+};
+</script>
