b8b904cd32a7d574c8af05c0c6c38b291ad04717
LowArte <47986420+LowArte@users.noreply.github.com>
2020-02-06 21:14:42 +0300

    Fix

---
+++ Work/app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Feedback;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'profilactic']);
+    }
+
+    /**
+     * Add new feedback to database
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function savefeedback(Request $request)
+    {
+        $feed = new Feedback();
+        $feed['user_id'] = Auth::user()['id'];
+        $feed['type'] = $request['type'];
+        $feed['text'] = $request['text'];
+        $feed['answered'] = 0;
+        $feed->save();
+
+        return response()->json(['success' => true]);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('feedback');
+    }
+}
