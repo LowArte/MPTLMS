623fc75e8001544dd109c6312874602ee6f4ea5a
SurgeonMPT <artem160920@mail.ru>
2020-02-13 15:18:30 +0300

    newTimetable

--- Work/resources/views/roles/admin/timetable.blade.php
+++ Work/resources/views/roles/admin/timetable.blade.php
@@ -1,30 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<v-tabs light mobile-break-point="0" grow>
-    <v-tab>
-        Расписание
-    </v-tab>
-    <v-tab>
-        Конструктор
-    </v-tab>
-    <v-tab-item>
-        <v-container fluid grid-list-md text-xs-center>
-            <c-panel :_components_prop="{{json_encode($panel_array) ?? null}}"></c-panel>
-            <c-timetable :_departaments_info="{{json_encode($departaments_info) ?? null}}" 
-                         :_groups_info="{{json_encode($groups_info) ?? null}}" 
-                         :_schedule="{{json_encode($schedule) ?? null}}"
-                         _slug="{{auth()->user()->post->slug}}"/>
-        </v-container>
-    </v-tab-item>
-    <v-tab-item>
-        <c-bild-timetable :_departaments_info="{{json_encode($departaments_info) ?? null}}" 
-                          :_groups_info="{{json_encode($groups_info) ?? null}}" 
-                          :_schedule_bild="{{json_encode($schedule_bild) ?? null}}"
-                          :_places="{{json_encode($places) ?? null}}"
-                          :_disciplines="{{json_encode($disciplines) ?? null}}"
-                          :_teachers="{{json_encode($teachers) ?? null}}"
-                          _slug="{{auth()->user()->post->slug}}"/>
-    </v-tab-item>
-</v-tabs>
+<v-container fluid grid-list-md text-xs-center>
+    <c-panel :_components_prop="{{json_encode($panel_array) ?? null}}"></c-panel>
+    <c-timetable :_departaments_info="{{json_encode($departaments_info) ?? null}}" 
+                    :_groups_info="{{json_encode($groups_info) ?? null}}" 
+                    :_schedule="{{json_encode($schedule) ?? null}}"
+                    :_schedule_bild="{{json_encode($schedule_bild) ?? null}}"
+                    :_places="{{json_encode($places) ?? null}}"
+                    :_disciplines="{{json_encode($disciplines) ?? null}}"
+                    :_teachers="{{json_encode($teachers) ?? null}}"
+                    _slug="{{auth()->user()->post->slug}}"/>
+</v-container>
 @endsection
