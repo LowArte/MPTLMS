fa9330a8f6c4c9486b31aef448f35ef144070ff7
SurgeonMPT <artem160920@mail.ru>
2020-03-24 12:28:16 +0300

    Revert "get"
    This reverts commit 06620c3d91734796228d79d12b32e6203a868bcd.

--- Work/.phpstorm.meta.php
+++ Work/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-23 12:42:25.
+    * Generated on 2020-03-08 18:42:14.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
