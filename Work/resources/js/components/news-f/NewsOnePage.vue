f224a19366cdd1924470d480daa424d23d97cea1
SurgeonMPT <artem160920@mail.ru>
2020-05-31 12:07:25 +0300

    fix

--- Work/resources/js/components/news-f/NewsOnePage.vue
+++ Work/resources/js/components/news-f/NewsOnePage.vue
@@ -2,7 +2,7 @@
 v-card.mx-auto.mb-3(max-width="600px" v-if="post")
     v-system-bar(color="primary" lights-out dark)
         router-link(class='nounderline' :to="'/' + user.post.slug + '/news'")
-            v-btn(block x-small) Ко всем новостям
+            v-btn.mx-auto(text x-small) Ко всем новостям
     v-carousel(height="300" min-height="250px" :continuous="false" :cycle="false" :show-arrows="false" delimiter-icon="mdi-minus" v-if="post.normal_images.length > 0")
         v-carousel-item(v-for="(mediain, i) in post.normal_images" :key="i" :src="mediain.url")
     v-system-bar(color="#F5F5F5")
@@ -22,13 +22,13 @@ v-card.mx-auto.mb-3(max-width="600px" v-if="post")
         v-row(v-for="(comment,i) in post.comments" :key="i")
             v-col(cols="2")
                 v-avatar(color='red' centered size="48")
-                    span {{comment.user.name[0].toUpper()}}
+                    span {{comment.user.name[0]}}
             v-col
                 span {{comment.text}}
 
     v-divider
     v-card-actions
-        v-btn(text @click="clickLike(post.id)")
+        v-btn(text @click="clickLike(post.id)" :loading="loading" :disabled="loading")
             v-icon {{!post.isLike ? 'favorite_border' : 'mdi-heart'}}
             span.mx-2 {{post.likes}}
 </template>
@@ -59,6 +59,7 @@ export default {
     data: () => {
         return {
             text:"",
+            loading: null,
             post: {
                 id: 0,
                 time: null,
@@ -85,7 +86,9 @@ export default {
     },
     methods: {
         async clickLike(id) {
-            this.$store.dispatch(actions.SET_LIKE,{userId:this.user.id,newsId:id})
+            this.loading = true;
+            await this.$store.dispatch(actions.SET_LIKE,{userId:this.user.id,newsId:id})
+            this.loading = false;
         },
         async sendComment(id) {
             this.$store.dispatch(actions.ADD_COMMENT,{text:this.text,id:id})
